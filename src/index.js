5ec094e45835093dcbc57b66b8f9159742fbc629
David Heinebäck <david.heineback@gmail.com>
2021-05-04 19:42:48 +0200

    add: global theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import GlobalStyle from './themes/GlobalStyle.js'
+import Theme from './themes/Theme.js'
+import { ThemeProvider } from 'styled-components'
 import App from './App'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={Theme}>
+    <GlobalStyle/>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
-);
+)
+
