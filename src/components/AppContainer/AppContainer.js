621ddafabc5b631c64bf4fb45572bb0e5ecdd325
Tamim Jabr <tj222kg@student.lnu.se>
2021-05-08 18:38:33 +0200

    seperat the style for app container

--- src/components/AppContainer/AppContainer.js
+++ src/components/AppContainer/AppContainer.js
@@ -1,14 +1,6 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Container from '@material-ui/core/Container'
-import styled  from 'styled-components'
-
-const StyledAppContainer = styled(Container)`
-  display:flex;
-  align-items:center;
-  justify-content:center;
-  min-height:100vh;
-`
+import {StyledAppContainer} from './StyledAppContainer'
 
 const AppContainer = ({children}) => {
 
