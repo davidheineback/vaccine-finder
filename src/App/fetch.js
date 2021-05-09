62f920fa6a91a4039ece5b985c54d9d107947045
David Heinebäck <du222aa@student.lnu.se>
2021-05-09 20:48:26 +0200

    refactor: fetch calls

--- src/App/fetch.js
+++ src/App/fetch.js
@@ -1,26 +1,23 @@
 // Module to make the fetch calls
 
-export async function getCliniques(setCliniques, county) {
+export async function getCliniques (setCliniques, county) {
   try {
     const categories = await fetch('https://booking-api.mittvaccin.se/clinique/')
     const response = await categories.json()
-    console.log(response)
-    const filteredResponse = response.filter(clinique => clinique.success_redirect_booking.toLowerCase().includes(county.toLowerCase()))
+    const filteredResponse = response.filter(clinique => clinique.style.toLowerCase().includes(county.toLowerCase()))
     setCliniques(filteredResponse)
   } catch (error) {
     throw new Error(error)
   }
 }
 
-export async function getAppointmentTypes(setAppointmentType, station) {
+export async function getAppointmentTypes (station) {
   console.log(station)
   try {
     const categories = await fetch(`https://booking-api.mittvaccin.se/clinique/${station}/appointmentTypes`)
     const response = await categories.json()
-    setAppointmentType(response)
+    return response
   } catch (error) {
     throw new Error(error)
   }
 }
-
-
