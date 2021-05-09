26c1c7b2cd29750af55dcb9b56393fe330878384
David Heinebäck <du222aa@student.lnu.se>
2021-05-09 19:11:15 +0200

    refactor changed name from component folder to Views

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
 import GlobalState from '../GlobalState/GlobalState'
-import Cliniques from '../components/Cliniques/Cliniques'
-import StartView from '../components/StartView/StartView'
+import Cliniques from '../Views/Cliniques/Cliniques'
+import StartView from '../Views/StartView/StartView'
 // import SearchView from '../components/SearchView/SearchView'
-import SelectAppointment from '../components/SelectAppointment/SelectAppointment'
-import AppContainer from '../components/AppContainer/AppContainer'
-import Wrapper from '../components/Wrapper/Wrapper'
+import SelectAppointment from '../Views/SelectAppointment/SelectAppointment'
+import AppContainer from '../Views/AppContainer/AppContainer'
+import Wrapper from '../Views/Wrapper/Wrapper'
 
 // REGION HALLAND // REGION VÄSTERNORRLAND // REGION KALMAR LÄN // REGION ÖSTERGÖTLAND
 
