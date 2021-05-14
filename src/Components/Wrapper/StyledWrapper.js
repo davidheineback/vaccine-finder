91ac2616bb5a4d261054f51d4603117b8a75412e
David Heinebäck <du222aa@student.lnu.se>
2021-05-14 11:38:10 +0200

    refactor: added margin to wrapper

--- src/Components/Wrapper/StyledWrapper.js
+++ src/Components/Wrapper/StyledWrapper.js
@@ -7,9 +7,10 @@ export const StyledWrapper = styled(Box)`
     rgba(148, 187, 233, 0.8813726174063375) 11%,
     rgba(174, 238, 181, 0.9009804605435925) 100%
   );
+  margin-top: 40px;
   min-height: 400px;
   min-width:300px;
-  max-width:60%;
+  max-width:30%;
   background: rgba(255, 255, 255, 0.4);
   box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
   backdrop-filter: blur(1.5px);
