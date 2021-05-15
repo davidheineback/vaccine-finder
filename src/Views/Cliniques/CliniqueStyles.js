38e7a6fec4c09d3868c18ec008eed0ee76900eff
David Heinebäck <du222aa@student.lnu.se>
2021-05-15 16:18:27 +0200

    refactor: add multiselect

--- src/Views/Cliniques/CliniqueStyles.js
+++ src/Views/Cliniques/CliniqueStyles.js
@@ -1,8 +1,11 @@
 import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded'
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import styled from 'styled-components'
 
-export const StyledHeartIcon = styled(FavoriteBorderRoundedIcon)`
+export const StyledBorderHeartIcon = styled(FavoriteBorderRoundedIcon)`
   color: rgba(240, 52, 52, 0.7);
 `
-
+export const StyledFilledHeartIcon = styled(FavoriteIcon)`
+  color: rgba(240, 52, 52, 0.7);
+`
 
